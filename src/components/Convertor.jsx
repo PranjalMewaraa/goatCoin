733e10e1c3754c53dc46b3aee942a34cd3acf7ed
PranjalMewaraa <mewarapranjal089@gmail.com>
2024-12-05 17:18:52 +0530

    sd

--- src/components/Convertor.jsx
+++ src/components/Convertor.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import DropdownItem from "./dropdown";
+import DropdownItem from "./Dropdown";
 
 const Convertor = () => {
   const [data, setData] = useState({
