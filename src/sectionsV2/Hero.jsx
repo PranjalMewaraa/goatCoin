ef6216a36ca8692ad3388deccb92fbf34592bc74
PranjalMewaraa <mewarapranjal089@gmail.com>
2024-12-25 17:55:28 +0530

    `Added width and height attributes to image element in Hero component`

--- src/sectionsV2/Hero.jsx
+++ src/sectionsV2/Hero.jsx
@@ -378,6 +378,8 @@ const Hero = () => {
                 src="/image2.jpg"
                 className="max-h-[600px] 2xl:max-h-[800px]"
                 alt="2"
+                width={800}
+                height={600}
               />
             </picture>
           </div>
