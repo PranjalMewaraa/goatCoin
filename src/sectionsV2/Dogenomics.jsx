767e6cb92f0bdcf6e9e684d87a80155d182e8309
PranjalMewaraa <mewarapranjal089@gmail.com>
2024-12-26 11:52:18 +0530

    asas

--- src/sectionsV2/Dogenomics.jsx
+++ src/sectionsV2/Dogenomics.jsx
@@ -11,7 +11,7 @@ const Dogenomics = () => {
         style={{ backgroundImage: "url(/.png)" }}
       >
         <img
-          src="/token.svg"
+          src="/token.png"
           className=" lg:max-h-[550px] 2xl:max-h-[800px] md:h-auto"
           alt="tokenomicds"
         />
